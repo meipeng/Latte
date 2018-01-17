a3cb1a642e7975e3a253530f2ccdbce828f478e4
meipeng <1148017085@qq.com>
2018-01-17 14:28:33 +0800

    no message

---
+++ latte_core/src/main/java/hztywl/com/latte/app/Configurator.java
@@ -0,0 +1,12 @@
+package hztywl.com.latte.app;
+
+import java.util.WeakHashMap;
+
+/**
+ * Created by ${梅鹏} on 2018/1/17.
+ */
+
+public class Configurator {
+
+    private static final WeakHashMap<String ,Object> LATTE_CONFIGS=new WeakHashMap<>();
+}
---
+++ latte_core/src/main/java/hztywl/com/latte/app/Latte.java
@@ -0,0 +1,10 @@
+package hztywl.com.latte.app;
+
+/**
+ * Created by ${梅鹏} on 2018/1/17.
+ */
+
+public final class Latte {
+
+
+}
